e3bf007b6b3c27b5af54ab721a438d6589df6257
steven <stevenbarrera750@gmail.com>
2020-06-19 22:58:15 -0600

    Inicios Oscuros 02

---
+++ FerreteriaProMAX/Controllers/DetalleEstadoesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FerreteriaProMAX.Models;
+
+namespace FerreteriaProMAX.Controllers
+{
+    public class DetalleEstadoesController : Controller
+    {
+        private FerreteriaDBEntities db = new FerreteriaDBEntities();
+
+        // GET: DetalleEstadoes
+        public ActionResult Index()
+        {
+            var detalleEstado = db.DetalleEstado.Include(d => d.Estado).Include(d => d.Empleado);
+            return View(detalleEstado.ToList());
+        }
+
+        // GET: DetalleEstadoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleEstado detalleEstado = db.DetalleEstado.Find(id);
+            if (detalleEstado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleEstado);
+        }
+
+        // GET: DetalleEstadoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdEstado = new SelectList(db.Estado, "IdEstado", "Estado1");
+            ViewBag.idEmpleado = new SelectList(db.Empleado, "IdEmpleado", "IdEmpleado");
+            return View();
+        }
+
+        // POST: DetalleEstadoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idDetalleEstado,IdEstado,FechaInicio,FechaFIN,idEmpleado")] DetalleEstado detalleEstado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DetalleEstado.Add(detalleEstado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdEstado = new SelectList(db.Estado, "IdEstado", "Estado1", detalleEstado.IdEstado);
+            ViewBag.idEmpleado = new SelectList(db.Empleado, "IdEmpleado", "IdEmpleado", detalleEstado.idEmpleado);
+            return View(detalleEstado);
+        }
+
+        // GET: DetalleEstadoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleEstado detalleEstado = db.DetalleEstado.Find(id);
+            if (detalleEstado == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdEstado = new SelectList(db.Estado, "IdEstado", "Estado1", detalleEstado.IdEstado);
+            ViewBag.idEmpleado = new SelectList(db.Empleado, "IdEmpleado", "IdEmpleado", detalleEstado.idEmpleado);
+            return View(detalleEstado);
+        }
+
+        // POST: DetalleEstadoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idDetalleEstado,IdEstado,FechaInicio,FechaFIN,idEmpleado")] DetalleEstado detalleEstado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(detalleEstado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdEstado = new SelectList(db.Estado, "IdEstado", "Estado1", detalleEstado.IdEstado);
+            ViewBag.idEmpleado = new SelectList(db.Empleado, "IdEmpleado", "IdEmpleado", detalleEstado.idEmpleado);
+            return View(detalleEstado);
+        }
+
+        // GET: DetalleEstadoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleEstado detalleEstado = db.DetalleEstado.Find(id);
+            if (detalleEstado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleEstado);
+        }
+
+        // POST: DetalleEstadoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DetalleEstado detalleEstado = db.DetalleEstado.Find(id);
+            db.DetalleEstado.Remove(detalleEstado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FerreteriaProMAX/Controllers/DetalleProvProdsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FerreteriaProMAX.Models;
+
+namespace FerreteriaProMAX.Controllers
+{
+    public class DetalleProvProdsController : Controller
+    {
+        private FerreteriaDBEntities db = new FerreteriaDBEntities();
+
+        // GET: DetalleProvProds
+        public ActionResult Index()
+        {
+            var detalleProvProd = db.DetalleProvProd.Include(d => d.proveedores);
+            return View(detalleProvProd.ToList());
+        }
+
+        // GET: DetalleProvProds/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleProvProd detalleProvProd = db.DetalleProvProd.Find(id);
+            if (detalleProvProd == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleProvProd);
+        }
+
+        // GET: DetalleProvProds/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdProveedores = new SelectList(db.proveedores, "IdProveedores", "Nombre");
+            return View();
+        }
+
+        // POST: DetalleProvProds/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdDetalleProvProd,Fecha,IdProveedores")] DetalleProvProd detalleProvProd)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DetalleProvProd.Add(detalleProvProd);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdProveedores = new SelectList(db.proveedores, "IdProveedores", "Nombre", detalleProvProd.IdProveedores);
+            return View(detalleProvProd);
+        }
+
+        // GET: DetalleProvProds/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleProvProd detalleProvProd = db.DetalleProvProd.Find(id);
+            if (detalleProvProd == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdProveedores = new SelectList(db.proveedores, "IdProveedores", "Nombre", detalleProvProd.IdProveedores);
+            return View(detalleProvProd);
+        }
+
+        // POST: DetalleProvProds/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdDetalleProvProd,Fecha,IdProveedores")] DetalleProvProd detalleProvProd)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(detalleProvProd).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdProveedores = new SelectList(db.proveedores, "IdProveedores", "Nombre", detalleProvProd.IdProveedores);
+            return View(detalleProvProd);
+        }
+
+        // GET: DetalleProvProds/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DetalleProvProd detalleProvProd = db.DetalleProvProd.Find(id);
+            if (detalleProvProd == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detalleProvProd);
+        }
+
+        // POST: DetalleProvProds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DetalleProvProd detalleProvProd = db.DetalleProvProd.Find(id);
+            db.DetalleProvProd.Remove(detalleProvProd);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
